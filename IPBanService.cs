18ec66130f6334c91abb40c43469f5569c3c4e5a
@ShargonXL <shargon@gmail.com>
2017-04-04 13:10:58 +0200

    Check as DNS (local network)

--- IPBanService.cs
+++ IPBanService.cs
@@ -267,11 +267,13 @@ namespace IPBan
                                             break;
                                         }
                                         // Check Host by name
+                                        Log.Write(LogLevel.Info, "Parse as Ip fail, checking dns {0}", tempIPAddress);
                                         IPHostEntry entry = Dns.GetHostEntry(tempIPAddress);
                                         if (entry != null && entry.AddressList != null && entry.AddressList.Length > 0)
                                         {
                                             ipAddress = entry.AddressList.FirstOrDefault().ToString();
                                             foundMatch = true;
+                                            Log.Write(LogLevel.Info, "Dns result {0} = {1}", tempIPAddress, ipAddress);
                                             break;
                                         }
                                     }
