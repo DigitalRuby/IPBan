3bc3940af6832768b310bec09b162a1c0be9179f
Jeff Johnson <jjxtra@gmail.com>
2018-05-23 15:04:11 -0600

    Fix inverted logic

--- IPBanService.cs
+++ IPBanService.cs
@@ -531,8 +531,8 @@ namespace IPBan
             string queryString = "<QueryList>";
             foreach (ExpressionsToBlockGroup group in Config.Expressions.Groups)
             {
-                if (group.MinimumWindowsMajorVersion > Environment.OSVersion.Version.Major ||
-                    (group.MinimumWindowsMajorVersion >= Environment.OSVersion.Version.Major && group.MinimumWindowsMinorVersion >= Environment.OSVersion.Version.Minor))
+                if (Environment.OSVersion.Version.Major > group.MinimumWindowsMajorVersion ||
+                    (Environment.OSVersion.Version.Major >= group.MinimumWindowsMajorVersion && Environment.OSVersion.Version.Minor >= group.MinimumWindowsMinorVersion))
                 {
                     ulong keywordsDecimal = ulong.Parse(group.Keywords.Substring(2), NumberStyles.AllowHexSpecifier, CultureInfo.InvariantCulture);
                     queryString += "<Query Id='" + (++id).ToString() + "' Path='" + group.Path + "'><Select Path='" + group.Path + "'>*[System[(band(Keywords," + keywordsDecimal.ToString() + "))]]</Select></Query>";
