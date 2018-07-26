312827935ee1f9aa1c8a0fa8e19f2401bffc1ee5
Jeff Johnson <jjxtra@gmail.com>
2018-07-26 10:45:34 -0600

    Use powershell remove dir command

--- CreatePackage.ps1
+++ CreatePackage.ps1
@@ -1,6 +1,6 @@
 param([Parameter(Mandatory=$true)] [String]$version)
 
-rm -r "./package"
+Remove-Item ./package -Recurse -ErrorAction Ignore
 dotnet publish IPBan.sln -f netcoreapp2.1 -o package/linux-x64 -c Release -r linux-x64
 dotnet publish IPBan.sln -f netcoreapp2.1 -o package/win-x86 -c Release -r win-x86
 Compress-Archive -Path ./package/linux-x64/* -DestinationPath ./package/IPBan-Linux-x64.zip
