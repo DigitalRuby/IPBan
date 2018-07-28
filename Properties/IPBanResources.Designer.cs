974a3a30630c0b08f9ec1521fad975a4b61b16c7
Jeff Johnson <jjxtra@gmail.com>
2018-07-28 15:05:46 -0600

    Make resources public

--- Properties/IPBanResources.Designer.cs
+++ Properties/IPBanResources.Designer.cs
@@ -22,7 +22,7 @@ namespace IPBan {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class IPBanResources {
+    public class IPBanResources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace IPBan {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("IPBan.Properties.IPBanResources", typeof(IPBanResources).Assembly);
@@ -51,7 +51,7 @@ namespace IPBan {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,7 +63,7 @@ namespace IPBan {
         /// <summary>
         ///   Looks up a localized string similar to 60E555E4ADFE4AAB9DAE61E0D57ADD7E.
         /// </summary>
-        internal static string IPBanKey1 {
+        public static string IPBanKey1 {
             get {
                 return ResourceManager.GetString("IPBanKey1", resourceCulture);
             }
