b3dd4b5f1c2e132640eb8dbd7a1325b2555ffbbd
Jeff Johnson <jjxtra@gmail.com>
2019-04-06 15:07:22 -0600

    Fix namespace

--- Properties/IPBanResources.Designer.cs
+++ Properties/IPBanResources.Designer.cs
@@ -39,7 +39,7 @@ namespace DigitalRuby.IPBan {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("IPBan.Properties.IPBanResources", typeof(IPBanResources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DigitalRuby.IPBan.Properties.IPBanResources", typeof(IPBanResources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
