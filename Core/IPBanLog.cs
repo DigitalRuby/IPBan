faf797ca513c5f39124805e747f0dc90a4dadaee
Jeff Johnson <jjxtra@gmail.com>
2019-04-17 11:41:48 -0600

    Disable logger under test, freezes travis ci

--- Core/IPBanLog.cs
+++ Core/IPBanLog.cs
@@ -30,6 +30,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Configuration;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 using NLog;
@@ -144,6 +145,18 @@ namespace DigitalRuby.IPBan
         {
             try
             {
+                foreach (Assembly assem in AppDomain.CurrentDomain.GetAssemblies())
+                {
+                    // Can't do something like this as it will load the nUnit assembly
+                    // if (assem == typeof(NUnit.Framework.Assert))
+
+                    if (assem.FullName.ToLowerInvariant().StartsWith("nunit.framework"))
+                    {
+                        // don't setup logger in test mode, it freezes travis ci
+                        return;
+                    }
+                }
+
                 LogFactory factory = null;
                 try
                 {
