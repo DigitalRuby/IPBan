8f2c543b22e56e3d70ede802f3564c52f30e1c9e
Jeff Johnson <jjxtra@gmail.com>
2019-04-17 11:53:05 -0600

    Running test as sudo freezes travis ci, not nlog

--- Core/IPBanLog.cs
+++ Core/IPBanLog.cs
@@ -145,18 +145,6 @@ namespace DigitalRuby.IPBan
         {
             try
             {
-                foreach (Assembly assem in AppDomain.CurrentDomain.GetAssemblies())
-                {
-                    // Can't do something like this as it will load the nUnit assembly
-                    // if (assem == typeof(NUnit.Framework.Assert))
-
-                    if (assem.FullName.ToLowerInvariant().StartsWith("nunit.framework"))
-                    {
-                        // don't setup logger in test mode, it freezes travis ci
-                        return;
-                    }
-                }
-
                 LogFactory factory = null;
                 try
                 {
