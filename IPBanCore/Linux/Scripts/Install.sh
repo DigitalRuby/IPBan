7936a002e3c5826fd344d4178e14460643c7a95e
Jeff Johnson <jjxtra@gmail.com>
2019-11-27 12:27:13 -0700

    Update Install.sh

--- IPBanCore/Linux/Scripts/Install.sh
+++ IPBanCore/Linux/Scripts/Install.sh
@@ -6,7 +6,7 @@ FILE_NAME="IPBan-Linux-x64_$VERSION_UNDERSCORES.zip"
 # sudo -i; bash <(wget -qO- https://raw.githubusercontent.com/DigitalRuby/IPBan/master/IPBanCore/Linux/Scripts/Install.sh)
 
 # install unzipper, install iptables and ipset, make folder /opt/ipban
-sudo apt-get -q -y install unzip; sudo apt-get install -q -y iptables; sudo apt-get install -q -y ipset; sudo apt-get -q -y update; sudo mkdir /opt/ipban; cd /opt/ipban;
+sudo apt-get -q -y install unzip; sudo apt-get install -q -y iptables; sudo apt-get install -q -y ipset; sudo apt-get -q -y update; sudo mkdir /opt/ipban -p; cd /opt/ipban;
 
 # download latest release and extract to /opt/ipban
 sudo wget https://github.com/DigitalRuby/IPBan/releases/download/$VERSION_DOTS/$FILE_NAME; unzip $FILE_NAME; rm $FILE_NAME;
