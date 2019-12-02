c15d20ce748183e0d9de32fde2f14934097c1f2f
Jeff Johnson <jjxtra@gmail.com>
2019-12-01 17:09:57 -0700

    Add byte array key for using byte arrays as keys in dictionaries

---
+++ IPBanCore/Core/Utility/ByteArrayKey.cs
@@ -0,0 +1,86 @@
+﻿/*
+MIT License
+
+Copyright (c) 2019 Digital Ruby, LLC - https://www.digitalruby.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigitalRuby.IPBanCore
+{
+    /// <summary>
+    /// Allows using byte array as a key
+    /// </summary>
+    public struct ByteArrayKey
+    {
+        /// <summary>
+        /// Bytes
+        /// </summary>
+        public byte[] Bytes { get; private set; }
+
+        private readonly int _hashCode;
+
+        private static int GetHashCode(Span<byte> bytes)
+        {
+            unchecked
+            {
+                int hash = 17;
+                foreach (byte element in bytes)
+                {
+                    hash = hash * 31 + element;
+                }
+                return hash;
+            }
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="bytes">Bytes</param>
+        public ByteArrayKey(byte[] bytes)
+        {
+            Bytes = bytes;
+            _hashCode = GetHashCode(bytes);
+        }
+
+        /// <summary>
+        /// Check if equal to other byte array key
+        /// </summary>
+        /// <param name="obj">Other byte array key</param>
+        /// <returns>True if equal, false otherwise</returns>
+        public override bool Equals(object obj)
+        {
+            var other = (ByteArrayKey)obj;
+            return Bytes.AsSpan().SequenceEqual(other.Bytes.AsSpan());
+        }
+
+        /// <summary>
+        /// Get hash code for the bytes
+        /// </summary>
+        /// <returns>Hash code</returns>
+        public override int GetHashCode()
+        {
+            return _hashCode;
+        }
+    }
+}
