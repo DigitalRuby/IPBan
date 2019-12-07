f55c94c79892d99d6cd4a1d2bb5e5d7e00b7a5fb
Jeff Johnson <jjxtra@gmail.com>
2019-12-07 14:21:27 -0700

    Update install.cmd

--- IPBanCore/Windows/Scripts/install.cmd
+++ IPBanCore/Windows/Scripts/install.cmd
@@ -1,4 +1,6 @@
 REM Make sure to extract service files to c:/ipban first!
+auditpol.exe /set /category:"{69979849-797A-11D9-BED3-505054503030}" /success:enable /failure:enable
+auditpol.exe /set /category:"{69979850-797A-11D9-BED3-505054503030}" /success:enable /failure:enable
 sc.exe create IPBAN type= own start= delayed-auto binPath= c:/ipban/DigitalRuby.IPBan.exe DisplayName= "IPBan Service"
 sc.exe description IPBAN "Automatically builds firewall rules for abusive login attempts: https://github.com/DigitalRuby/IPBan"
 sc.exe start IPBAN
