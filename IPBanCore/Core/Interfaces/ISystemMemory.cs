ffb2465b4ce1709ab9319e6bb819c8cd783b781a
Jeff Johnson <jjxtra@gmail.com>
2020-02-29 09:50:28 -0700

    Regex tweaks

--- IPBanCore/Core/Interfaces/ISystemMemory.cs
+++ IPBanCore/Core/Interfaces/ISystemMemory.cs
@@ -102,8 +102,8 @@ namespace DigitalRuby.IPBanCore
                         using StreamReader reader = File.OpenText("/proc/meminfo");
                         string total = reader.ReadLine();
                         string available = reader.ReadLine();
-                        Match totalMatch = Regex.Match(total, "[0-9]+", RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.CultureInvariant);
-                        Match availableMatch = Regex.Match(available, "[0-9]+", RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.CultureInvariant);
+                        Match totalMatch = Regex.Match(total, "[0-9]+", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+                        Match availableMatch = Regex.Match(available, "[0-9]+", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
                         totalMemory = long.Parse(totalMatch.Value, CultureInfo.InvariantCulture) * 1024;
                         availableMemory = long.Parse(availableMatch.Value, CultureInfo.InvariantCulture) * 1024;
                         return true;
--- IPBanCore/Windows/IPBanWindowsEventViewer.cs
+++ IPBanCore/Windows/IPBanWindowsEventViewer.cs
@@ -42,7 +42,7 @@ namespace DigitalRuby.IPBanCore
     {
         private static readonly Regex invalidXmlRegex = new Regex(@"(?<![\uD800-\uDBFF])[\uDC00-\uDFFF]|[\uD800-\uDBFF](?![\uDC00-\uDFFF])|[\x00-\x08\x0B\x0C\x0E-\x1F\x7F-\x9F\uFEFF\uFFFE\uFFFF]", RegexOptions.Compiled);
 
-        private IIPBanService service;
+        private readonly IIPBanService service;
         private EventLogQuery query;
         private EventLogWatcher watcher;
         private string previousQueryString;
