b2d2341f89d9cad868d251954dcd76deb0cd5c90
Jeff Johnson <jjxtra@gmail.com>
2020-05-26 11:45:00 -0600

    Tweak wording for lists

--- IPBanCore/IPBanResources.Designer.cs
+++ IPBanCore/IPBanResources.Designer.cs
@@ -773,7 +773,7 @@ namespace DigitalRuby.IPBanCore {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to IP addresses, cidr masks, dns names or user names to always ban and never unban. Comma separated..
+        ///   Looks up a localized string similar to IP addresses, cidr masks, dns names or user names to always ban and never unban..
         /// </summary>
         public static string IPBlacklistTooltip {
             get {
@@ -809,7 +809,7 @@ namespace DigitalRuby.IPBanCore {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to IP addresses, cidr masks or dns names to never ban. Comma separated..
+        ///   Looks up a localized string similar to IP addresses, cidr masks or dns names to never ban..
         /// </summary>
         public static string IPWhitelistTooltip {
             get {
