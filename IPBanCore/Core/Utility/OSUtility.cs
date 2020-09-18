d10339e5fc4cea4aa943fa3fa1c716ee1aa62db9
Jeff Johnson <jjxtra@gmail.com>
2020-09-18 12:20:54 -0600

    Remove unused method

--- IPBanCore/Core/Utility/OSUtility.cs
+++ IPBanCore/Core/Utility/OSUtility.cs
@@ -449,22 +449,6 @@ namespace DigitalRuby.IPBanCore
         }
 
         /// <summary>
-        /// Check if this process is running on Windows in an in process instance in IIS
-        /// </summary>
-        /// <returns>True if Windows and in an in process instance on IIS, false otherwise</returns>
-        public static bool IsRunningInProcessIIS()
-        {
-            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                return false;
-            }
-
-            string processName = Path.GetFileNameWithoutExtension(Process.GetCurrentProcess().ProcessName);
-            return (processName.Contains("w3wp", StringComparison.OrdinalIgnoreCase) ||
-                processName.Contains("iisexpress", StringComparison.OrdinalIgnoreCase));
-        }
-
-        /// <summary>
         /// Add app domain exception handlers
         /// </summary>
         /// <param name="domain">Appdomain</param>
