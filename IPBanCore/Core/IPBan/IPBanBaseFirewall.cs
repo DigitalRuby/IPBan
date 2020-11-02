60cc81a92b4d4d312f9e639df410e607e7c5a08b
Jeff Johnson <jjxtra@gmail.com>
2020-11-02 13:30:53 -0700

    Supress finalizer

--- IPBanCore/Core/IPBan/IPBanBaseFirewall.cs
+++ IPBanCore/Core/IPBan/IPBanBaseFirewall.cs
@@ -22,6 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+using System;
 using System.Collections.Generic;
 using System.Security.Policy;
 using System.Threading;
@@ -91,6 +92,7 @@ namespace DigitalRuby.IPBanCore
         {
             if (!Disposed)
             {
+                GC.SuppressFinalize(this);
                 Disposed = true;
                 OnDispose();
             }
