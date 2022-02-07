640f6d33bfe00a90629ae0eb6af77152417f0224
Jeff Johnson <jjxtra@gmail.com>
2022-02-07 14:12:21 -0700

    Copyright range

--- IPBanCore/IPBanResources.Designer.cs
+++ IPBanCore/IPBanResources.Designer.cs
@@ -19,7 +19,7 @@ namespace DigitalRuby.IPBanCore {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class IPBanResources {
@@ -1609,7 +1609,7 @@ namespace DigitalRuby.IPBanCore {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to (c) 2018 Digital Ruby, LLC.
+        ///   Looks up a localized string similar to (c) 2018-{0} Digital Ruby, LLC.
         /// </summary>
         public static string SiteCopyright {
             get {
