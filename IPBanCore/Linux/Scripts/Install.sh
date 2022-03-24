93d3c63c66cd1a202eb34f230d3ce582b5f21a23
Jeff Johnson <jjxtra@gmail.com>
2022-03-24 15:25:37 -0600

    Update Install.sh

--- IPBanCore/Linux/Scripts/Install.sh
+++ IPBanCore/Linux/Scripts/Install.sh
@@ -8,7 +8,7 @@
 # To uninstall: sudo systemctl stop ipban; sudo systemctl disable ipban; sudo rm /opt/ipban -r
 #
 
-VERSION_DOTS="1.7.0"
+VERSION_DOTS="1.7.1"
 VERSION_UNDERSCORES=${VERSION_DOTS//./_}
 FILE_NAME="IPBan-Linux-x64_$VERSION_UNDERSCORES.zip"
 
