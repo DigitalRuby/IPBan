9a59219654991cc643881c8552c2364239c56bc0
Alan Vazquez <30251559+KT-Mir@users.noreply.github.com>
2022-07-27 15:47:12 -0700

    Update uninstall.cmd (#209)
    * Update uninstall.cmd
    
    Remove "-p" as it's not a valid flag for mkdir on Windows and use the backslash for proper syntax.
    
    * Update uninstall.cmd
    
    Co-authored-by: Jeff Johnson <jjxtra@gmail.com>

--- IPBanCore/Windows/Scripts/uninstall.cmd
+++ IPBanCore/Windows/Scripts/uninstall.cmd
@@ -1,6 +1,6 @@
 sc.exe stop IPBAN
 sc.exe delete IPBAN
-mkdir -p c:/ipban
-mkdir -p "C:/Program Files/IPBan"
-rmdir /s /q c:/ipban
-rmdir /s /q "C:/Program Files/IPBan"
+if not exist "C:\ipban" mkdir "C:\ipban"
+if not exist "C:\Program Files\IPBan" mkdir "C:\Program Files\IPBan"
+rmdir /s /q c:\ipban
+rmdir /s /q "C:\Program Files\IPBan"
