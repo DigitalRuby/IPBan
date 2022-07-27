409c8d4359bf828b316e59214948bd42cb6de17e
Jeff Johnson <jjxtra@gmail.com>
2022-07-27 16:47:06 -0600

    Update uninstall.cmd

--- IPBanCore/Windows/Scripts/uninstall.cmd
+++ IPBanCore/Windows/Scripts/uninstall.cmd
@@ -1,6 +1,6 @@
 sc.exe stop IPBAN
 sc.exe delete IPBAN
-mkdir c:\ipban
-mkdir "C:\Program Files\IPBan"
+if not exist "C:\ipban" mkdir "C:\ipban"
+if not exist "C:\Program Files\IPBan" mkdir "C:\Program Files\IPBan"
 rmdir /s /q c:\ipban
 rmdir /s /q "C:\Program Files\IPBan"
