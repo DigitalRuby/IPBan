1c67832c7d38d74ece53b46145c69770d269316d
Jeff Johnson <jjxtra@gmail.com>
2024-01-09 13:56:31 -0700

    Update install_latest.ps1

--- IPBanCore/Windows/Scripts/install_latest.ps1
+++ IPBanCore/Windows/Scripts/install_latest.ps1
@@ -28,7 +28,7 @@ if ($PSVersionTable.PSVersion.Major -lt 5 -or ($PSVersionTable.PSVersion.Major -
     exit -1
 }
 
-$VERSION_DOTS = "1.9.0"
+$VERSION_DOTS = "2.0.0"
 $VERSION_UNDERSCORES = $VERSION_DOTS -replace "\.","_"
 $FILE_NAME = "IPBan-Windows-x64_$VERSION_UNDERSCORES.zip"
 $INSTALL_PATH = "C:/Program Files/IPBan"
