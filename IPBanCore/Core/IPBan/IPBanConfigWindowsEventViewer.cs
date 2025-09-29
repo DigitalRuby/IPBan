41cc57533129b5ea63d250d77f56930ca3ae3a2c
Jeff Johnson <jjxtra@gmail.com>
2025-09-29 11:29:42 -0600

    Add description field to log files and event viewer

--- IPBanCore/Core/IPBan/IPBanConfigWindowsEventViewer.cs
+++ IPBanCore/Core/IPBan/IPBanConfigWindowsEventViewer.cs
@@ -328,6 +328,15 @@ namespace DigitalRuby.IPBanCore
         [LocalizedDisplayName(nameof(IPBanResources.DisableNotificationFlags))]
         [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public IPAddressNotificationFlags NotificationFlags { get; set; } = IPAddressNotificationFlags.None;
+
+        /// <summary>
+        /// Description
+        /// </summary>
+        [DisplayFormat(ConvertEmptyStringToNull = false)]
+        [Required(AllowEmptyStrings = true)]
+        [LocalizedDisplayName(nameof(IPBanResources.Description))]
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string Description { get; set; } = string.Empty;
     }
 
     /// <summary>
--- IPBanCore/IPBanResources.Designer.cs
+++ IPBanCore/IPBanResources.Designer.cs
@@ -790,6 +790,15 @@ namespace DigitalRuby.IPBanCore {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Description.
+        /// </summary>
+        public static string Description {
+            get {
+                return ResourceManager.GetString("Description", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Details.
         /// </summary>
         public static string Details {
